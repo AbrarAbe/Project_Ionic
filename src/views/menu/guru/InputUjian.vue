ef3e7c73857dffe3a116c5ed4ef94ba6a393349c
AbrarAbe <abrar.abe01@gmail.com>
2024-06-28 08:53:56 +0700

    modified:   src/views/menu/guru/InputUjian.vue

--- src/views/menu/guru/InputUjian.vue
+++ src/views/menu/guru/InputUjian.vue
@@ -1,52 +1,86 @@
 <template>
   <ion-page>
-    <ion-content class="home-page">
-      <div class="main_content_div">
-        <ion-header mode="ios">
-          <ion-toolbar>
-            <ion-back-button slot="start" default-href="/" />
-            <ion-title>Menu Input Ujian</ion-title>
-          </ion-toolbar>
-        </ion-header>
-        <ion-content class="ion-padding" :fullscreen="true" :scrollEvents="true">
-          <!-- Form untuk menambahkan siswa -->
-          <ion-item>
-            <ion-label>Soal :</ion-label>
-            <ion-input v-model="namaSiswa" placeholder="Masukkan soal ujian"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label>a.</ion-label>
-            <ion-input v-model="namaSiswa" placeholder="Masukkan pilihan ganda"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label>b.</ion-label>
-            <ion-input v-model="namaSiswa" placeholder="Masukkan pilihan ganda"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label>c.</ion-label>
-            <ion-input v-model="namaSiswa" placeholder="Masukkan pilihan ganda"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label>d.</ion-label>
-            <ion-input v-model="namaSiswa" placeholder="Masukkan pilihan ganda"></ion-input>
-          </ion-item>
-          <ion-button @click="tambahSiswa">Tambah Ujian</ion-button>
-        </ion-content>
-      </div>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Input Soal</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-label position="stacked">Pertanyaan</ion-label>
+          <ion-input v-model="pertanyaan" placeholder="Masukkan pertanyaan"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Jawaban A</ion-label>
+          <ion-input v-model="pilihanA" placeholder="Masukkan jawaban A"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Jawaban B</ion-label>
+          <ion-input v-model="pilihanB" placeholder="Masukkan jawaban B"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Jawaban C</ion-label>
+          <ion-input v-model="pilihanC" placeholder="Masukkan jawaban C"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Jawaban D</ion-label>
+          <ion-input v-model="pilihanD" placeholder="Masukkan jawaban D"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Jawaban Benar</ion-label>
+          <ion-select v-model="jawabanBenar">
+            <ion-select-option value="A">A</ion-select-option>
+            <ion-select-option value="B">B</ion-select-option>
+            <ion-select-option value="C">C</ion-select-option>
+            <ion-select-option value="D">D</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
+      <ion-button expand="block" @click="submitSoal">Submit</ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup>
-//import { onMounted } from "vue";
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonButton,
-  IonPage,
-  IonContent,
-} from "@ionic/vue";
+import { ref } from 'vue';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '@/views/firebase.js';
+
+const pertanyaan = ref('');
+const pilihanA = ref('');
+const pilihanB = ref('');
+const pilihanC = ref('');
+const pilihanD = ref('');
+const jawabanBenar = ref('');
+
+const submitSoal = async () => {
+  if (pertanyaan.value && pilihanA.value && pilihanB.value && pilihanC.value && pilihanD.value && jawabanBenar.value) {
+    try {
+      await addDoc(collection(db, 'soal'), {
+        pertanyaan: pertanyaan.value,
+        answers: {
+          A: pilihanA.value,
+          B: pilihanB.value,
+          C: pilihanC.value,
+          D: pilihanD.value,
+        },
+        jawabanBenar: jawabanBenar.value,
+      });
+      alert('Soal berhasil disimpan');
+      pertanyaan.value = '';
+      pilihanA.value = '';
+      pilihanB.value = '';
+      pilihanC.value = '';
+      pilihanD.value = '';
+      jawabanBenar.value = '';
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  } else {
+    alert('Semua field harus diisi');
+  }
+};
 </script>
 
 <style scoped>
